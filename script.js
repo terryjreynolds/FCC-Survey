6f5f2a210763f0652aff91a5fab6f02128f3e498
Terry Reynolds <terrancejreynolds@gmail.com>
2018-10-15 14:49:01 -0400

    init repo

---
+++ script.js
@@ -0,0 +1,7 @@
+document
+  .getElementById("submit")
+  .addEventListener("click", sayThanksWithARickRoll);
+
+function sayThanksWithARickRoll() {
+  window.open("https://www.youtube.com/watch?v=oHg5SJYRHA0");
+}
